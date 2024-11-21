ca7a966ef011df161f6e67b02e863e3b11a1bb18
Michael Owooje <michaelowooje@gmail.com>
2024-11-20 21:46:36 -0500

    added user/clothingItems schema

---
+++ models/clothingItems.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const clothingItemsSchema = new mongoose.Schema({
+  name: { type: String, required: true, minlength: 2, maxlength: 30 },
+
+  weather: {
+    type: String,
+    required: true,
+    enum: {
+      values: ["hot", "warm", "cold"],
+    },
+  },
+
+  imageUrl: {
+    type: String,
+    required: [true, "The avatar field is required"],
+    validate: {
+      validator(value) {
+        return validator.isURL(value);
+      },
+    },
+    message: "You must enter a valid URL",
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+
+  likes: {
+    type: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+    required: true,
+    default: [],
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("item", clothingItemsSchema);
