c67fd645dfc129eb57a55fc32ea8d92091c2c9fc
Michael Owooje <michaelowooje@gmail.com>
2024-12-12 19:16:29 -0500

    deleted getuser and getusers

--- controllers/users.js
+++ controllers/users.js
@@ -10,40 +10,6 @@ const {
 } = require("../utils/errors");
 const { JWT_SECRET } = require("../utils/config");
 
-// GET /users
-
-const getUsers = (req, res) => {
-  User.find({})
-    .then((users) => res.status(200).send(users))
-    .catch((err) => {
-      console.error(err);
-      return res
-        .status(DEFAULT)
-        .send({ message: "An error has occurred on the server" });
-    });
-};
-
-// GET /users/:userId
-
-const getUser = (req, res) => {
-  const { userId } = req.params;
-  User.findById(userId)
-    .orFail()
-    .then((user) => res.status(200).send(user))
-    .catch((err) => {
-      console.error(err);
-      if (err.name === "DocumentNotFoundError") {
-        return res.status(NOT_FOUND).send({ message: err.message });
-      }
-      if (err.name === "CastError") {
-        return res.status(BAD_REQUEST).send({ message: err.message });
-      }
-      return res
-        .status(DEFAULT)
-        .send({ message: "An error has occurred on the server" });
-    });
-};
-
 const getCurrentUser = (req, res) => {
   const userId = req.user._id;
 
@@ -161,9 +127,7 @@ const login = async (req, res) => {
 };
 
 module.exports = {
-  getUsers,
   createUser,
-  getUser,
   getCurrentUser,
   login,
   updateUserProfile,
