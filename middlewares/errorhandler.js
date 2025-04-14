27226261f1988c76bf3265c49ed92a555b74305a
Michael Owooje <michaelowooje@gmail.com>
2025-04-14 15:30:22 -0400

    added next to errorhandler

--- middlewares/errorhandler.js
+++ middlewares/errorhandler.js
@@ -1,11 +1,12 @@
 const { DEFAULT } = require("../utils/errors");
 
-const errorHandler = (err, req, res) => {
+const errorHandler = (err, req, res, next) => {
   const { statusCode = DEFAULT, message } = err;
   res.status(statusCode).send({
     message:
       statusCode === DEFAULT ? "An error occurred on the server" : message,
   });
+  next();
 };
 
 module.exports = errorHandler;
