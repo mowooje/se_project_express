83919657d9772361106b57bc824ba63f72cbd44c
Michael Owooje <michaelowooje@gmail.com>
2025-04-14 02:50:47 -0400

    set up server crash testing

--- app.js
+++ app.js
@@ -15,6 +15,13 @@ app.use(cors());
 // Log every request
 app.use(requestLogger);
 
+// Crash test route
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
+
 // Unprotected routes
 app.use("/", mainRouter);
 
